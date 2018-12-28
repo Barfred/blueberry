391068bacc136b844eb560bf7aaa327adac6725d
Mikael Barfred <mikael@barfred.net>
2018-12-28 01:44:47 +0100

    Updates to sandbox

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,7 @@
 ssh pi@raspberrypi '
 cd /home/pi/blueberry
 rm -rf virtual_env
-python3 -m venv virtual_env
+python3 -m venv --system-site-packages virtual_env
 . virtual_env/bin/activate
 pip install -r requirements.txt
 '
--- sandbox.py
+++ sandbox.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python3
 
+from bluesound.bluesound_subscribe import SubscriptionObject
+from bluesound.bluesound_subscription_objects import secondsInTrack
+from bluesound.bluesound_control import Bluesound
+import pifacecad
+
 def handelTitle1(new_title):
     print("Title1: %s" % new_title)
+    cad.lcd.write("Title1:{}".format(new_title))
+
 
 def handelTitle2(new_title):
     print("Title2: %s" % new_title)
+    cad.lcd.write("Title2:{}".format(new_title))
 
 
-from bluesound.bluesound_subscribe import SubscriptionObject
 title1 = SubscriptionObject(['status', 'title1'], handelTitle1)
-title2 = SubscriptionObject(['status', 'title2'], handelTitle1)
+title2 = SubscriptionObject(['status', 'title2'], handelTitle2)
 
-from bluesound.bluesound_subscription_objects import secondsInTrack
 
 def handelSecondsInTrack(sec):
     print("Seconds: %s" % sec)
 
+
+cad = pifacecad.PiFaceCAD()
+cad.lcd.blink_off()
+cad.lcd.cursor_off()
+cad.lcd.backlight_on()
+
 secondsInTrack.setCallback(handelSecondsInTrack)
 
-from bluesound.bluesound_control import Bluesound
-bluos = Bluesound("192.168.88.23", 1.0, set([title1, title2, secondsInTrack]))
+bluos = Bluesound("192.168.88.25", 1.0, set([title1, title2, secondsInTrack]))
 bluos.start()
 
 input("Press enter to play..")
