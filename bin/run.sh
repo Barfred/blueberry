35b8a052e852ee2abc042c46294328e30dbd4ca6
Mikael Barfred <mikael@barfred.net>
2019-02-10 15:43:45 +0100

    Add systemd config file

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,5 @@
 ssh pi@192.168.88.238 '
-sudo killall python3
-ps -ef | grep sandbox.py
-cd /home/pi/blueberry
-. virtual_env/bin/activate
-PYTHONPATH=./bluesound ./blueberry.py
+sudo systemctl stop blueberry
+ps -ef | grep blueberry.py
+sudo systemctl start blueberry
 '
